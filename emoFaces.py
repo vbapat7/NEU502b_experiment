48ac53af28fb5447ebe7dd8ca2850a4e48926659
Bing Li <bingli@princeton.edu>
2025-03-04 17:21:18 -0500

    working day 1 script

--- emoFaces.py
+++ emoFaces.py
@@ -42,7 +42,7 @@ from psychopy.hardware import keyboard
 MR_settings = {
     'TR': 2.000,     # duration (sec) per whole-brain volume
     'volumes': 5,    # number of whole-brain 3D volumes per scanning run
-    'sync': '5', # character to use as the sync timing event; assumed to come at start of a volume
+    'sync': 'equal', # character to use as the sync timing event; assumed to come at start of a volume
     'skip': 0,       # number of volumes lacking a sync pulse at start of scan (for T1 stabilization)
 #    'sound': True    # in test mode: play a tone as a reminder of scanner noise # Commented 2024
     }
